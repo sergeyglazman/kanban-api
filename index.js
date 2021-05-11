439c0eb862e5714a94e7fc0540c01e00fdf2e4bf
Sergey.Glazman <sergeyglazman@gmail.com>
2021-05-11 23:09:53 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const routes = require("./routes");
 
 const app = express()
 const PORT = process.env.PORT || 5000;
@@ -10,29 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-let cards = [{id: "1", name: 'My card 1', status: 'done', priority: 1},
-    {id: "2", name: 'My card 2', status: 'to do', priority: 2}];
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.get('/card', (req, res) => {
-    res.send(cards)
-})
-
-app.delete('/card/:cardId', (req, res) => {
-    const cardId = req.params.cardId;
-    cards = cards.filter(el => el.id !== cardId);
-    res.send(cards);
-})
-
-app.patch('/card/:cardId', (req, res) => {
-    const cardId = req.params.cardId;
-    const card = req.body;
-    cards = cards.map(el => el.id === cardId ? ({ id: el.id, ...card }) : el)
-  res.send('Card updated');
-})
+routes(app);
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
---
+++ routes.js
@@ -0,0 +1,30 @@
+let cards = [{id: "1", name: 'My card 1', status: 'done', priority: 1},
+    {id: "2", name: 'My card 2', status: 'to do', priority: 2}];
+
+
+function routes(app) {
+
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    })
+
+    app.get('/card', (req, res) => {
+        res.send(cards)
+    })
+
+    app.delete('/card/:cardId', (req, res) => {
+        const cardId = req.params.cardId;
+        cards = cards.filter(el => el.id !== cardId);
+        res.send(cards);
+    })
+
+    app.patch('/card/:cardId', (req, res) => {
+        const cardId = req.params.cardId;
+        const card = req.body;
+        cards = cards.map(el => el.id === cardId ? ({id: el.id, ...card}) : el)
+        res.send('Card updated');
+    });
+}
+
+module.exports = routes;
