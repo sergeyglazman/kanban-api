fb81a2361c8c351e66910c95c7c07fd5f065d474
Sergey.Glazman <sergeyglazman@gmail.com>
2021-05-13 22:56:19 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const routes = require("./routes");
 const bodyParser = require("./bodyParser");
+const dbConnection = require("./dbConnection");
 
 const app = express()
 const PORT = process.env.PORT || 5000;
 
+dbConnection();
 bodyParser(app);
 routes(app);
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
 })
+
+
+//mongodb+srv://Sergey:6Afe3f2739!@cluster0.ojq9p.mongodb.net/kanban?retryWrites=true&w=majority
--- routes.js
+++ routes.js
@@ -1,36 +1,19 @@
-let cards = [{id: "1", name: 'My card 1', status: 'done', priority: 1},
-    {id: "2", name: 'My card 2', status: 'to do', priority: 2}];
+const cardCreate = require("./card/cardCreate");
+const cardDelete = require("./card/cardDelete");
+const cardGetAll = require("./card/cardGetAll");
+const cardUpdateById = require("./card/cardUpdateById");
 
 
 function routes(app) {
-
 
     app.get('/', (req, res) => {
         res.send('Hello World!')
     })
 
-    app.get('/card', (req, res) => {
-        res.send(cards)
-    })
-
-    app.delete('/card/:cardId', (req, res) => {
-        const cardId = req.params.cardId;
-        cards = cards.filter(el => el.id !== cardId);
-        res.send(cards);
-    })
-
-    app.post('/card', (req, res) => {
-        const card = req.body;
-        cards.push({ id: Math.random().toString(), ...card });
-        res.send('Card created')
-    })
-
-    app.patch('/card/:cardId', (req, res) => {
-        const cardId = req.params.cardId;
-        const card = req.body;
-        cards = cards.map(el => el.id === cardId ? ({id: el.id, ...card}) : el)
-        res.send('Card updated');
-    });
+    app.get('/card', cardGetAll);
+    app.delete('/card/:cardId', cardDelete);
+    app.post('/card', cardCreate);
+    app.patch('/card/:cardId', cardUpdateById);
 }
 
 module.exports = routes;
