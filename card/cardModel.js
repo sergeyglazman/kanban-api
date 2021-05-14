14141de5b537a37ebbb321c59e7554b996c23ee7
Sergey.Glazman <sergeyglazman@gmail.com>
2021-05-14 22:08:33 +0100

    new elements

---
+++ card/cardModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const cardSchema = new Schema({
+    name: {
+        type: String,
+        select: true,
+        required: true,
+        unique: false,
+    },
+    description: {
+        type: String,
+        select: true,
+        required: true,
+    },
+    status: {
+        type: String,
+        select: true,
+        required: true,
+    },
+    priority: {
+        type: Number,
+        select: true,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('Card', cardSchema)
+
--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const routes = require("./routes");
 const bodyParser = require("./bodyParser");
 const dbConnection = require("./dbConnection");
+const cors = require("./cors");
 
 const app = express()
 const PORT = process.env.PORT || 5000;
 
 dbConnection();
 bodyParser(app);
+cors(app);
 routes(app);
 
 app.listen(PORT, () => {
