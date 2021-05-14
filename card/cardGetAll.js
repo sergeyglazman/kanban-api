5479da6d8fb9fbfa31ae5e7795220c990eba18ed
Sergey.Glazman <sergeyglazman@gmail.com>
2021-05-14 22:46:21 +0100

    added card get all

--- card/cardGetAll.js
+++ card/cardGetAll.js
@@ -1,5 +1,16 @@
+const Card = require('./cardModel')
+
+
 function cardGetAll(req, res)  {
-    res.send('All cards')
+    Card.find()
+        .exec()
+        .then((result) => {
+            res.status(200).json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json('Card get all error')
+        })
 }
 
 module.exports = cardGetAll;
