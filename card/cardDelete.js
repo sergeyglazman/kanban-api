d6899966c118cdbe8e59a28586d2fc8449fa64d5
Sergey.Glazman <sergeyglazman@gmail.com>
2021-05-14 23:24:56 +0100

    card delete by id added

--- card/cardDelete.js
+++ card/cardDelete.js
@@ -1,5 +1,17 @@
+const Card = require('./cardModel')
+
 function cardDelete(req, res)  {
-    res.send('Card deleted');
+    const cardId = req.params.cardId;
+
+    Card.deleteOne({_id: cardId})
+        .exec()
+        .then((result) => {
+            res.status(200).json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json('Card delete error')
+        })
 }
 
 module.exports = cardDelete;
