1be52cd0cf69bda8ed383859bed5ad4c6ebd8354
Sergey.Glazman <sergeyglazman@gmail.com>
2021-05-14 23:35:30 +0100

    card update by id added

--- card/cardUpdateById.js
+++ card/cardUpdateById.js
@@ -1,5 +1,17 @@
+const Card = require('./cardModel');
+
 function cardUpdateById(req, res)  {
-    res.send('Card updated');
+    const cardId = req.params.cardId;
+
+    Card.updateOne({ _id: cardId }, req.body)
+        .exec()
+        .then(() => {
+            res.status(200).json('Card was successfully updated')
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json('Card update error');
+        })
 }
 
 module.exports = cardUpdateById;
